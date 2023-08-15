c3c3c9c6870c58157071548aff54e6523e0ef9fb
WalterGit <walteraxalta@gmail.com>
2023-08-15 01:46:51 -0300

    Definindo as propriedades da entidade categoria

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,7 +2,15 @@ import Category from "./category";
 
 describe("Category Tests", () => {
   test("should be category is Movie", () => {
-    const category = new Category('Movie');
-    expect(category.name).toBe('Movie');
+    const category = new Category({
+      name: "Movie",
+      description: "Movie description",
+      is_asctive: true,
+      created_at: new Date(),
+    });
+    expect(category.props.name).toBe("Movie");
+    expect(category.props.description).toBe("Movie description");
+    expect(category.props.is_asctive).toBe(true);
+    expect(category.props.created_at).toBeInstanceOf(Date);
   });
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,32 @@
+import { get } from "sortablejs";
+
+export type CategoryProperties = {
+  name: string;
+  description: string;
+  is_asctive: boolean;
+  created_at?: Date;
+};
+
 export default class Category {
-  constructor(public name: string) {}
+  constructor(public readonly props: CategoryProperties) {}
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  set name(name: string) {
+    this.props.name = name;
+  }
+
+  get description(): string {
+    return this.props.description;
+  }
+
+  get is_active(): boolean {
+    return this.props.is_asctive;
+  }
+
+  get created_at(): Date | undefined {
+    return this.props.created_at;
+  }
 }
